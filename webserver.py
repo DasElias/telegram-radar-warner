22c59c40a4f3cbcb20c8432f3279c35d95b5dca8
Elias Horner <elias.horner@student.htldornbirn.at>
2021-12-14 14:30:00 +0100

    feat: message truncation

--- webserver.py
+++ webserver.py
@@ -11,18 +11,31 @@ from shared import all_messages, confirmation_code_queue
 
 tz = timezone(os.getenv("TIMEZONE"))
 default_elems_returned = int(os.getenv("DEFAULT_ELEMS_RETURNED"))
+max_message_length = int(os.getenv("MAX_MESSAGE_LENGTH"))
 
 def getMessageById(id):
   elem = next((m for m in all_messages if m.id == id), None)
   return getattr(elem, "message", None)
 
+def truncate_message(msg):
+  if msg is None:
+    return None
+
+  # 3 characters are appended
+  truncate_behind = max_message_length + 3
+  if(len(msg) > truncate_behind):
+    msg = msg[:truncate_behind] + "..."  
+    
+  return msg
+
 def mapMessage(msg):
   date = msg.date.replace(tzinfo=pytz.utc).astimezone(tz)
+  truncated_orig_message = truncate_message(msg.message)
 
   # Build reply message
   reply_message = None
   if msg.reply_to is not None:
-    reply_message = getMessageById(msg.reply_to.reply_to_msg_id)
+    reply_message = truncate_message(getMessageById(msg.reply_to.reply_to_msg_id))
   
   # Build parsed message
   parsed_message = date.strftime("%H:%M") + "\t "
@@ -30,10 +43,10 @@ def mapMessage(msg):
   if reply_message is not None:
     parsed_message += "Antwort auf\"" + reply_message + "\"\t "
 
-  parsed_message += msg.message
+  parsed_message += truncated_orig_message
 
   return {
-    "originalMessage": msg.message,
+    "originalMessage": truncated_orig_message,
     "id": msg.id,
     "date": date,
     "replyMessage": reply_message,
