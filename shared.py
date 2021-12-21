64cf62147c3056e7a20fcf357559e6872295603c
Elias Horner <elias.horner@student.htldornbirn.at>
2021-12-21 13:54:05 +0100

    perf: has_message_replies optimization

--- shared.py
+++ shared.py
@@ -65,8 +65,8 @@ def has_message_replies(msg):
       return True
 
     # there can't be replies before the message was sent  
-    # if m.id == msg.id:
-    #  break
+    if m.id == msg.id:
+      break
   return False            
 
 def was_message_answered_by_admin(msg):
